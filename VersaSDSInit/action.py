acfde2b092bd762ea27a41d478eee42221c766cc
mattie <mattie.feng@feixitek.com>
2021-05-11 17:16:22 +0800

    Finished the function of setting IP on device

--- VersaSDSInit/action.py
+++ VersaSDSInit/action.py
@@ -377,9 +377,9 @@ class IpService(object):
     def __init__(self, conn=None):
         self.conn = conn
 
-    def set_ip(self, ip, device, netmask=24):
+    def set_ip(self, device, ip, gateway, netmask=24):
         connection_name = 'vtel_' + device
-        cmd = f"nmcli connection add con-name {connection_name} type ethernet ifname {device} ipv4.addresses {ip}/{netmask} ipv4.method manual"
+        cmd = f"nmcli connection add con-name {connection_name} type ethernet ifname {device} ipv4.addresses {ip}/{netmask} ipv4.gateway {gateway} ipv4.method manual"
         utils.exec_cmd(cmd, self.conn)
 
     def up_ip_service(self, device):
@@ -387,7 +387,7 @@ class IpService(object):
         cmd = f"nmcli connection up id {connection_name}"
         utils.exec_cmd(cmd, self.conn)
 
-    def modify_ip(self, device, new_ip, netmask=24):
+    def modify_ip(self, device, new_ip, gateway, netmask=24):
         connection_name = 'vtel_' + device
-        cmd = f"nmcli connection modify {connection_name} ipv4.address {new_ip}/{netmask}"
+        cmd = f"nmcli connection modify {connection_name} ipv4.address {new_ip}/{netmask} ipv4.gateway {gateway}"
         utils.exec_cmd(cmd, self.conn)
