6908241ae563f896ab521f67c04fcbc612572d1e
mattie <mattie.feng@feixitek.com>
2021-07-26 17:54:55 +0800

    初始配置工具功能分离（设置root密码以及允许root用户登录，连接IP设置），待测试

---
+++ BasicSetting/control.py
@@ -0,0 +1,55 @@
+# -*- coding:utf-8 -*-
+import action
+import sys
+
+
+def all_deploy(conf_args):
+    """一键执行完成软件安装以及配置、root密码设置、允许以root用户登录、连接IP的设置的功能"""
+    install = action.InstallSoftware(conf_args["User password"])
+    root_config = action.RootConfig(conf_args["User password"])
+    ip_service = action.IpService(conf_args["User password"])
+    ssh_service = action.OpenSSHService(conf_args["User password"])
+    print("\nPrepare to install software...")
+    if install.update_apt():
+        print("Start to install openssh-server")
+        if install.install_software("openssh-server"):
+            print(" Start openssh service")
+            if ssh_service.oprt_ssh_service("start"):
+                print("Set can be logged as root")
+                if root_config.set_root_permit_login():
+                    print("Set root password")
+                    root_config.set_root_password(conf_args["Root password"])
+                    print(" Restart openssh service")
+                    ssh_service.oprt_ssh_service("restart")
+        print("Start to install network-manager")
+        if install.install_software("network-manager"):
+            print(" Start to set network-manager config")
+            if install.set_nmcli_config():
+                print(f"Set {conf_args['IP']} on the {conf_args['Device']}")
+                if ip_service.set_local_ip(conf_args['Device'], conf_args['IP'], conf_args['Gateway']):
+                    ip_service.up_local_ip_service(conf_args['Device'])
+        print("\n")
+    else:
+        sys.exit()
+
+
+def set_root_pwd_permit_login(conf_args):
+    """root密码设置、允许以root用户登录"""
+    ssh_service = action.OpenSSHService(conf_args["User password"])
+    root_config = action.RootConfig(conf_args["User password"])
+    print("Set can be logged as root")
+    if root_config.set_root_permit_login():
+        print(" Start to set root password")
+        root_config.set_root_password(conf_args["Root password"])
+        print(" Start to restart openssh service")
+        ssh_service.oprt_ssh_service("restart")
+        print("Finish to set root password and can be logged as root")
+
+
+def set_local_ip(conf_args):
+    """连接IP的设置的功能"""
+    ip_service = action.IpService(conf_args["User password"])
+    print(f"Start to set {conf_args['IP']} on the {conf_args['Device']}")
+    if ip_service.set_local_ip(conf_args['Device'], conf_args['IP'], conf_args['Gateway']):
+        ip_service.up_local_ip_service(conf_args['Device'])
+        print(f"Finish to set {conf_args['IP']} on the {conf_args['Device']}")
--- BasicSetting/main.py
+++ BasicSetting/main.py
@@ -1,7 +1,8 @@
 # -*- coding:utf-8 -*-
 import argparse
 import sys
-import action
+import control
+import utils
 
 sys.path.append('../')
 import consts
@@ -10,10 +11,13 @@ import consts
 class InputParser(object):
 
     def __init__(self):
-        self.parser = argparse.ArgumentParser(description="Basic Setting of System")
+        self.parser = argparse.ArgumentParser(description="Basic Setting for operating system")
         self.setup_parser()
+        self.conf_args = {}
 
     def setup_parser(self):
+
+        subp = self.parser.add_subparsers(metavar='', dest='subargs_vtel')
 
         self.parser.add_argument('-v',
                                  '--version',
@@ -21,110 +25,143 @@ class InputParser(object):
                                  help='Show current version',
                                  action='store_true')
 
-        self.parser.add_argument('-ip',
-                                 '--ip',
-                                 dest='ip',
-                                 help='IP address',
-                                 action='store')
+        self.parser_build = subp.add_parser("build", aliases=['b'], help="Install software and set basic configuration")
 
-        self.parser.add_argument('-d',
-                                 '--device',
-                                 dest='device',
-                                 help='Device',
-                                 action='store')
+        self.parser_build.add_argument('-ip',
+                                       '--ip',
+                                       dest='ip',
+                                       help='IP address',
+                                       action='store')
 
-        self.parser.add_argument('-g',
-                                 '--gateway',
-                                 dest='gateway',
-                                 help='Gateway',
-                                 action='store')
+        self.parser_build.add_argument('-d',
+                                       '--device',
+                                       dest='device',
+                                       help='Device',
+                                       action='store')
 
-        self.parser.add_argument('-p',
-                                 '--password',
-                                 dest='password',
-                                 help='Password of current user',
-                                 action='store')
-        self.parser.add_argument('-r',
-                                 '--rootpwd',
-                                 dest='rootpwd',
-                                 help='Password of root that you want to set',
-                                 action='store')
+        self.parser_build.add_argument('-g',
+                                       '--gateway',
+                                       dest='gateway',
+                                       help='Gateway',
+                                       action='store')
 
-        self.parser.set_defaults(func=self.run_fun)
+        self.parser_build.add_argument('-p',
+                                       '--password',
+                                       dest='password',
+                                       help='Password of current user',
+                                       action='store')
+        self.parser_build.add_argument('-r',
+                                       '--rootpwd',
+                                       dest='rootpwd',
+                                       help='Password of root that you want to set',
+                                       action='store')
 
-    def collect_args(self, args):
-        conf_args = {}
-        ip = args.ip if args.ip else self.guide_check("IP", "10.203.1.78")
-        device = args.device if args.device else self.guide_check("Device", "ens160")
-        default_gateway = f"{'.'.join(ip.split('.')[:3])}.1"
-        gateway = args.gateway if args.gateway else self.guide_check("Gateway", default_gateway)
-        passwword = args.password if args.password else self.guide_check("User password", "password")
-        rootpwd = args.rootpwd if args.rootpwd else self.guide_check("Root password", "password")
+        self.parser_ip = subp.add_parser("ip", help="Connection IP settings")
 
-        conf_args["IP"] = ip
-        conf_args["Device"] = device
-        conf_args["Gateway"] = gateway
-        conf_args["User password"] = passwword
-        conf_args["Root password"] = rootpwd
-        print(conf_args)
+        self.parser_ip.add_argument('-ip',
+                                    '--ip',
+                                    dest='ip',
+                                    help='IP address',
+                                    action='store')
 
-        return conf_args
+        self.parser_ip.add_argument('-d',
+                                    '--device',
+                                    dest='device',
+                                    help='Device',
+                                    action='store')
 
-    def guide_check(self, target, default):
-        for i in range(3):
-            a = input(f"Input the value of '{target}' (default [{default}]): ")
-            if a.strip() == "":
-                return default
-            elif a.strip() == "exit":
+        self.parser_ip.add_argument('-g',
+                                    '--gateway',
+                                    dest='gateway',
+                                    help='Gateway',
+                                    action='store')
+
+        self.parser_ip.add_argument('-p',
+                                    '--password',
+                                    dest='password',
+                                    help='Password of current user',
+                                    action='store')
+
+        self.parser_root = subp.add_parser("root", aliases=['r'],
+                                           help="Set the root password and allow the root user to log in")
+
+        self.parser_root.add_argument('-r',
+                                      '--rootpwd',
+                                      dest='rootpwd',
+                                      help='Password of root that you want to set',
+                                      action='store')
+        self.parser_root.add_argument('-p',
+                                      '--password',
+                                      dest='password',
+                                      help='Password of current user',
+                                      action='store')
+
+        self.parser_build.set_defaults(func=self.run_func)
+        self.parser_ip.set_defaults(func=self.ip_func)
+        self.parser_root.set_defaults(func=self.root_func)
+        self.parser.set_defaults(func=self.help_usage)
+
+    def collect_ip_args(self, args):
+        ip = args.ip if args.ip else utils.guide_check("IP", "10.203.1.78")
+        if ip:
+            default_gateway = f"{'.'.join(ip.split('.')[:3])}.1"
+            gateway = args.gateway if args.gateway else utils.guide_check("Gateway", default_gateway)
+            if not gateway:
                 sys.exit()
-            else:
-                if target in ["Gateway", "IP"]:
-                    if action.check_ip(a):
-                        return a
-                    else:
-                        print(f"Please check the format of {target}.Enter again or Press CTRL+C to quit")
-                else:
-                    return a
+            device = args.device if args.device else utils.guide_check("Device", "ens160")
+        else:
+            sys.exit()
 
-    def run_fun(self, args):
+        self.conf_args["IP"] = ip
+        self.conf_args["Gateway"] = gateway
+        self.conf_args["Device"] = device
+
+    def collect_root_args(self, args):
+        rootpwd = args.rootpwd if args.rootpwd else utils.guide_check("Root password", "password")
+        self.conf_args["Root password"] = rootpwd
+
+    def collect_args(self, args, type):
+        if type == "all":
+            self.collect_ip_args(args)
+            self.collect_root_args(args)
+        if type == "ip":
+            self.collect_ip_args(args)
+        if type == "root":
+            self.collect_root_args(args)
+        password = args.password if args.password else utils.guide_check("User password", "password")
+        self.conf_args["User password"] = password
+
+        print(f"\nConfiguration: {self.conf_args}")
+
+    def run_func(self, args):
+        self.check_ip_and_gateway(args)
+        self.collect_args(args, "all")
+        control.all_deploy(self.conf_args)
+
+    def ip_func(self, args):
+        self.check_ip_and_gateway(args)
+        self.collect_args(args, "ip")
+        control.set_local_ip(self.conf_args)
+
+    def root_func(self, args):
+        self.collect_args(args, "root")
+        control.set_root_pwd_permit_login(self.conf_args)
+
+    def check_ip_and_gateway(self, args):
+        if args.ip:
+            if not utils.check_ip(args.ip):
+                print("\nPlease check the format of IP...\n")
+                sys.exit()
+        if args.gateway:
+            if not utils.check_ip(args.gateway):
+                print("\nPlease check the format of Gateway...\n")
+                sys.exit()
+
+    def help_usage(self, args):
         if args.version:
             print(f'Version: {consts.VERSION}')
         else:
-            if args.ip:
-                if not action.check_ip(args.ip):
-                    print("\nPlease check the format of IP...\n")
-                    sys.exit()
-            if args.gateway:
-                if not action.check_ip(args.gateway):
-                    print("\nPlease check the format of Gateway...\n")
-                    sys.exit()
-            conf_args = self.collect_args(args)
-            install = action.InstallSoftware(conf_args["User password"])
-            root_config = action.RootConfig(conf_args["User password"])
-            ip_service = action.IpService(conf_args["User password"])
-            ssh_service = action.OpenSSHService(conf_args["User password"])
-            print("\nPrepare to install software...")
-            if install.update_apt():
-                print("Start to install openssh-server")
-                if install.install_software("openssh-server"):
-                    print(" Start openssh service")
-                    if ssh_service.oprt_ssh_service("start"):
-                        print("Set can be logged as root")
-                        if root_config.set_root_permit_login():
-                            print("Set root password")
-                            root_config.set_root_password(conf_args["Root password"])
-                            print(" Restart openssh service")
-                            ssh_service.oprt_ssh_service("restart")
-                print("Start to install network-manager")
-                if install.install_software("network-manager"):
-                    print(" Start to set network-manager config")
-                    if install.set_nmcli_config():
-                        print(f"Set {conf_args['IP']} on the {conf_args['Device']}")
-                        if ip_service.set_local_ip(conf_args['Device'], conf_args['IP'], conf_args['Gateway']):
-                            ip_service.up_local_ip_service(conf_args['Device'])
-                print("\n")
-            else:
-                sys.exit()
+            self.parser.print_help()
 
     def parse(self):  # 调用入口
         args = self.parser.parse_args()
